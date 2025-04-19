27c304995cba69f32f225ab7ff3dba9652cf1bd2
Mason <masonyil@foxmail.com>
2025-04-20 00:24:53 +0100

    - Finished RemoveDuplicatesSln using only a pointer and a value memory cache
    - Finished Remove Duplications 2, but the result is not optimised, let's check the solution section

---
+++ JourneyOfLeetCodeTop150InterviewQs/RemoveDuplicatesFromSortedArraySln.cs
@@ -0,0 +1,26 @@
+﻿namespace JourneyOfLeetCodeTop150InterviewQs;
+public class RemoveDuplicatesFromSortedArraySln
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        //Run time beats 100%, Memory usage beats 99.23%
+        //weird that sbyte would cost more memory than a int
+        //could do it using hashset, but it is already sorted
+        //we only need to have a value copy to make sure the next one
+        //is not equal to the current one, then change using pointer(counter)
+        //the counter is doing the same thing compare to removeElementSln
+        //count from very beginning
+        if (nums.Length == 0)
+            return 0;
+        int counter = 1;
+        int currentValue = nums[0];
+        for (int i = 1; i < nums.Length; i++) 
+        {
+            if (nums[i] != currentValue) 
+            {
+                nums[counter++] = currentValue = nums[i];
+            }
+        }
+        return counter;
+    }
+}
---
+++ JourneyOfLeetCodeTop150InterviewQs/RemoveDuplicatesFromSortedArraySlnII.cs
@@ -0,0 +1,30 @@
+﻿namespace JourneyOfLeetCodeTop150InterviewQs;
+public class RemoveDuplicatesFromSortedArraySlnII
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        //Run time beats 34.66%, Memory usage beats 88.25%
+        if (nums.Length == 0)
+            return 0;
+        int counter = 1;
+        int currentValue = nums[0];
+        bool alreadyTwice = false;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != currentValue)
+            {
+                nums[counter++] = currentValue = nums[i];
+                alreadyTwice = false;
+            }
+            else 
+            {
+                if (!alreadyTwice) 
+                {
+                    nums[counter++] = nums[i];
+                    alreadyTwice = true;
+                }
+            }
+        }
+        return counter;
+    }
+}
