9d2b3dbfbb242f972e86e9da0bb4be2c2c537e15
Mason <masonyil@foxmail.com>
2025-04-20 13:37:23 +0100

    - Update result from solution, where we still use counter and find a nature such that a number should not be equal to counter - 2 because it is already a sorted array

--- JourneyOfLeetCodeTop150InterviewQs/RemoveDuplicatesFromSortedArraySlnII.cs
+++ JourneyOfLeetCodeTop150InterviewQs/RemoveDuplicatesFromSortedArraySlnII.cs
@@ -3,7 +3,20 @@ public class RemoveDuplicatesFromSortedArraySlnII
 {
     public int RemoveDuplicates(int[] nums)
     {
-        //Run time beats 34.66%, Memory usage beats 88.25%
+        if (nums.Length < 2)
+            return nums.Length;
+        //we dont need to worry about the first two elements
+        int counter = 2;
+        for (int i = 2; i < nums.Length; i++)
+        {
+            //it is already sorted.
+            if (nums[i] != nums[counter - 2])
+            {
+                nums[counter++] = nums[i];
+            }
+        }
+        return counter;
+        /*//Run time beats 34.66%, Memory usage beats 88.25%
         if (nums.Length == 0)
             return 0;
         int counter = 1;
@@ -25,6 +38,6 @@ public class RemoveDuplicatesFromSortedArraySlnII
                 }
             }
         }
-        return counter;
+        return counter;*/
     }
 }
