a0eb0519611d8b8f9341922b7438a2561014f21f
Mason <masonyil@foxmail.com>
2025-04-20 18:48:24 +0100

    - Update for Majority element, since the element will always exist for more than  n/2 times, so the worst scenario is that the counter is finally = 1 due to the other elements occupied other slots

---
+++ JourneyOfLeetCodeTop150InterviewQs/MajorityElementSln.cs
@@ -0,0 +1,24 @@
+﻿namespace JourneyOfLeetCodeTop150InterviewQs;
+public class MajorityElementSln
+{
+    public int MajorityElement(int[] nums)
+    {
+        //because the major element will always be
+        //more than half of the array, so the counter will at least be one once the whole array is traversed for the major element
+        int maj = 0;
+        int count = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (count == 0)
+            {
+                maj = nums[i];
+            }
+
+            count += nums[i] == maj ? 1 : -1;
+        }
+        return maj;
+        /*return nums.GroupBy(i => i)
+            .Where(g => g.Count() > (nums.Length / 2))
+            .First().Key;*/
+    }
+}
